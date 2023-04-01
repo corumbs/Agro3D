1c173ed3bf47529c97c5f3d51ac9e3e59042c36c
corumbs <ultimatecorumba@gmail.com>
2023-04-01 11:13:34 -0300

    gps follow map

--- app/src/main/java/com/example/bing/MainActivity.java
+++ app/src/main/java/com/example/bing/MainActivity.java
@@ -7,11 +7,12 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -31,9 +32,11 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 
 
+
 //libraries
 
 public class MainActivity extends AppCompatActivity {
+
     private Thread listenThread;
 
     private static final String DEVICE_ADDRESS = "10:52:1C:69:3E:6E"; // MAC address of the Bluetooth device
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothDevice device;
     private BluetoothSocket socket;
     private InputStream inputStream;
+    private Marker mMarker;
 
     private TextView deviceNameTextView;
     private TextView nmeaDataTextView;
@@ -65,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         IMapController mapController = map.getController();
-        mapController.setZoom(20.0);
+        mapController.setZoom(15.0);
         GeoPoint startPoint = new GeoPoint( -24.050397, -52.384150);
         mapController.setCenter(startPoint);
         marker = new Marker(map);
@@ -142,28 +146,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void listenForData() {
+
         try {
             inputStream = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
             while (bluetoothConnected) {
                 String data = reader.readLine();
-                if (data != null && data.startsWith("$GPGGA")) {
-                    // Split the data by commas
-                    String[] parts = data.split(",");
+                if (data != null) {
+                    runOnUiThread(() -> nmeaDataTextView.setText(data));
 
-                    // Extract the latitude and longitude
-                    if (parts.length >= 10) {
-                        double latitude = parseLatitude(parts[2], parts[3]);
-                        double longitude = parseLongitude(parts[4], parts[5]);
 
-                        // Update the map with the new position
+                    String[] sentenceParts = data.split(",");
+                    String sentenceType = sentenceParts[0].substring(3);
+                    // Check if the NMEA sentence is of the correct type
+                    if (sentenceType.equals("GGA")) {
+                        System.out.println("nmea: " + data);
+                        // Extract latitude and longitude values from the sentence
+                        double latitude = Double.parseDouble(sentenceParts[2].substring(0, 2));
+                        latitude += Double.parseDouble(sentenceParts[2].substring(2)) / 60;
+                        if (sentenceParts[3].equals("S")) {
+                            latitude = -latitude;
+                        }
+
+                        double longitude = Double.parseDouble(sentenceParts[4].substring(0, 3));
+                        longitude += Double.parseDouble(sentenceParts[4].substring(3)) / 60;
+                        if (sentenceParts[5].equals("W")) {
+                            longitude = -longitude;
+                        }
+
+
+                        // Use a latitude e longitude extraídas aqui
+                        System.out.println("Latitude: " + latitude + " " + sentenceParts[3]);
+                        System.out.println("Longitude: " + longitude + " " + sentenceParts[5]);
+
+                        // Display latitude and longitude on the screen
+                        double finalLatitude = latitude;
+                        double finalLongitude = longitude;
+
                         runOnUiThread(() -> {
-                            GeoPoint newPosition = new GeoPoint(latitude, longitude);
-                            map.getController().animateTo(newPosition);
-                            marker.setPosition(newPosition);
+
+                            nmeaDataTextView.setText(data);
+                            TextView latitudeTextView = findViewById(R.id.latitude);
+                            TextView longitudeTextView = findViewById(R.id.longitude);
+                            latitudeTextView.setText(String.format("%.4f", finalLatitude));
+                            longitudeTextView.setText(String.format("%.4f", finalLongitude));
+
+                            // Update marker position on the map
+                            GeoPoint newPoint = new GeoPoint(finalLatitude, finalLongitude);
+                            marker.setPosition(newPoint);
+                            map.getController().animateTo(newPoint);
                         });
                     }
+                } else {
+                    break;
                 }
             }
         } catch (IOException e) {
@@ -185,27 +221,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private double parseLatitude(String latitude,String hemisphere) {
-        double value = Double.parseDouble(latitude.substring(0, 2)) +
-                Double.parseDouble(latitude.substring(2)) / 60.0;
 
-        if (hemisphere.equals("S")) {
-            value *= -1.0;
-        }
 
-        return value;
-    }
-
-    private double parseLongitude(String longitude, String hemisphere) {
-        double value = Double.parseDouble(longitude.substring(0, 3)) +
-                Double.parseDouble(longitude.substring(3)) / 60.0;
-
-        if (hemisphere.equals("W")) {
-            value *= -1.0;
-        }
-
-        return value;
-    }
 
 }
 
